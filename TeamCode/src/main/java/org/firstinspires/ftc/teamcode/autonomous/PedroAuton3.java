92f83c2a521cb560b450965dcebadc94fbb8ed88
sreyanghosh <sreyan.ghosh.25@gmail.com>
2025-11-30 10:56:49 -0800

    Change line 12

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/PedroAuton3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/PedroAuton3.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.autonomous;
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.PathChain;
 import com.pedropathing.util.Timer;
@@ -9,29 +10,35 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
-public class PedroAuton2 extends OpMode {
+public class PedroAuton3 extends OpMode {
 
     private Follower follower;
     private Timer pathTimer, opModeTimer;
-    public enum PathState {GO_LAUNCH1, SHOOT_PRELOAD}
+    public enum PathState {GO_LAUNCH, SHOOT_PRELOAD, INTAKE_ONE, INTAKE_TWO, INTAKE_THREE}
     PathState pathState;
 
     private final Pose startPose = new Pose(81.20127795527158, 134.56869009584665, Math.toRadians(180));
     private final Pose launchPose = new Pose(81.20127795527158, 78, Math.toRadians(45));
+    private final Pose intakeOne = new Pose(102.36421725239616, 83.73162939297124, Math.toRadians(0));
 
-    private PathChain goLaunch;
+    private PathChain goLaunch, intakeFirst;
 
     public void buildPaths() {
         goLaunch = follower.pathBuilder()
-                .addPath(new BezierCurve(startPose, launchPose))
+                .addPath(new BezierLine(startPose, launchPose))
                 .setLinearHeadingInterpolation(startPose.getHeading(), launchPose.getHeading())
+                .build();
+
+        intakeFirst = follower.pathBuilder()
+                .addPath(new BezierLine(launchPose, intakeOne))
+                .setLinearHeadingInterpolation(launchPose.getHeading(), intakeOne.getHeading())
                 .build();
     }
 
     public void statePathUpdate() {
         switch (pathState) {
 
-            case GO_LAUNCH1:
+            case GO_LAUNCH:
                 follower.followPath(goLaunch, true);
                 setPathState(PathState.SHOOT_PRELOAD);
                 break;
@@ -40,7 +47,11 @@ public class PedroAuton2 extends OpMode {
                     // Flywheel logic
                     telemetry.addLine("Path Done");
                 }
+                setPathState(PathState.INTAKE_ONE);
                 break;
+            case INTAKE_ONE:
+
+
             default:
                 telemetry.addLine("No State Commanded");
                 break;
@@ -54,7 +65,7 @@ public class PedroAuton2 extends OpMode {
 
     @Override
     public void init() {
-        pathState = PathState.GO_LAUNCH1;
+        pathState = PathState.GO_LAUNCH;
         pathTimer = new Timer();
         opModeTimer = new Timer();
         follower = Constants.createFollower(hardwareMap);
